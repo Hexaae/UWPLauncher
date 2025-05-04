ba8f649382598c2ed8f6e9a4d1c1eabf0ab3a2a3
Hexaae <hexaae@gmail.com>
2025-05-04 15:13:31 +0200

    Update UWPLauncher.cpp

--- UWPLauncher.cpp
+++ UWPLauncher.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        MessageBoxA(NULL, "Usage: launchUWP.exe <PackageFamilyName>", "Error", MB_OK | MB_ICONERROR);
+        MessageBoxA(NULL, "Usage: launchUWP.exe <FullPackageFamilyName>", "Error", MB_OK | MB_ICONERROR);
         return 1;
     }
 
